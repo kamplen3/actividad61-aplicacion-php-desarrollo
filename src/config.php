f5bda89b900a8ebb7469515bf527e65d86bb8cf1
Ubuntu <ubuntu@ip-172-31-13-196.ec2.internal>
2026-03-03 13:01:06 +0000

    Correccion de config.php

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'db';
-$db   = getenv('MYSQL_DATABASE');
-$user = getenv('MYSQL_USER');
-$pass = getenv('MYSQL_PASSWORD');
+$db   = getenv('MARIADB_DATABASE');
+$user = getenv('MARIADB_USER');
+$pass = getenv('MARIADB_PASSWORD');
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
